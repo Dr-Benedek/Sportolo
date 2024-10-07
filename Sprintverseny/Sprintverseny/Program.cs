6b91eaa93256196c8e4cc28dd8f076b53483c2ec
Benedek <66177741+Dr-Benedek@users.noreply.github.com>
2024-10-07 10:32:57 +0200

    Add files via upload

---
+++ Sprintverseny/Sprintverseny/Program.cs
@@ -0,0 +1,34 @@
+﻿using Sprintverseny;
+using System.Text;
+List<Sprint> Sportolo = [];
+
+
+using StreamReader sr = new(path: "forras.txt", encoding: Encoding.UTF8);
+
+while (!sr.EndOfStream) Sportolo.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"{Sportolo.Count()} db versenyző fejezte be a versenyt!");
+
+var linq1 = Sportolo.Count(s => s.Kateg == "elit");
+Console.WriteLine($"Versenyzők száma az elit kategóriában: {linq1}");
+
+var linq2 = Sportolo.Where(s => s.Sex == "n").Average(s => s.Age);
+Console.WriteLine($"Női versenyzők átlag életkora: {linq2:00}");
+
+var linq3 = Sportolo.Sum(s => s.times["BikeTime"].TotalHours);
+Console.WriteLine($"A versenyzők kerékpározással töltött ideje: {linq3:0.00} óra");
+
+var linq4 = Sportolo.FindAll(s => s.Kateg == "elit junior").Average(s => s.times["SwimTime"].TotalMinutes);
+Console.WriteLine($"Átlagos úszás idő, elit junior kategóriában: {linq4:0.00} perc");
+
+//var linq5
+
+
+var linq6 = Sportolo.GroupBy(s => s.Kateg);
+Console.WriteLine("kategória szerint csoportosítva a versenyt befejezők száma: ");
+foreach (var group in linq6.OrderBy(g => g.Key))
+{
+    Console.WriteLine($"\t{group.Key} ({group.Count()} db)");
+}
+
+//linq++
---
+++ Sprintverseny/Sprintverseny/Sprint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprintverseny
+{
+    internal class Sprint
+    {
+        public Dictionary<string, TimeSpan> times = new Dictionary<string, TimeSpan>();
+        public string Name { get; set; }
+        public int BirthDate { get; set; }
+        public int StartNumber { get; set; }
+        public string Sex { get; set; }
+        public string Kateg { get; set; }
+        public int Age => (int)(DateTime.Now.Year - BirthDate);
+        public Sprint(string row)
+        {
+            var v = row.Split(';');
+            Name = v[0];
+            BirthDate = int.Parse(v[1]);
+            StartNumber= int.Parse(v[2]);
+            Sex = v[3];
+            Kateg = v[4];
+            times.Add("SwimTime", TimeSpan.Parse(v[5]));
+            times.Add("FirstDepo", TimeSpan.Parse(v[5]));
+            times.Add("BikeTime", TimeSpan.Parse(v[7]));
+            times.Add("SecondDepo", TimeSpan.Parse(v[8]));
+            times.Add("RunTime", TimeSpan.Parse(v[9]));
+        }
+    }
+}
+
